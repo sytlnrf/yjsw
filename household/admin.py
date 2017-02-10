413c5b1498d768489943d38cd098b613962f1f79
sytlnrf <sytlnrf@gmail.com>
2017-02-10 15:03:57 +0800

    refactor

--- household/admin.py
+++ household/admin.py
@@ -5,6 +5,9 @@ from .models import Users, Watermeter, Fees
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('u_name', 'u_address', 'created_at')
     list_filter = ['created_at']
+    search_fields = ['u_name', 'u_phone']
+    list_per_page = 1
+    date_hierarchy = 'created_at'
 admin.site.register(Users, UsersAdmin)
 admin.site.register(Watermeter)
 admin.site.register(Fees)
